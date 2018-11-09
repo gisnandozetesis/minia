9c8161bbff16c958a11ffa1e76f68b29835b44ed
Gisnando Kfuri <gisnando@zetesis.net>
2018-11-09 15:46:01 -0200

    Mensagem por TextInput.

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { spaceSearchResult } from '../actions';
 import * as firebase from 'firebase';
 import uuid from 'react-native-uuid';
+import { purple, white } from '../utils/colors';
 
 class Chat extends React.Component {
 
     state = {
         messages: [],
-        newMessage: 'teste de msg enviada'
+        newMessage: ''
     }
 
     componentWillMount() {
@@ -72,6 +73,10 @@ class Chat extends React.Component {
                 sender: 'usr01',
                 timestamp: Date.now()
             });
+
+            this.setState({
+                newMessage: ''
+            });
         }
     }
 
@@ -80,14 +85,14 @@ class Chat extends React.Component {
         const { messages } = this.state;
 
         return (
-            <View>
+            <View style={{ flex: 1, flexDirection: 'column', alignItems: 'flex-start' }}>
                 {messages.map(msg => (<Text key={msg.id}>{msg.content}</Text>))}
 
-                <View>
-                    <TextInput value={this.state.newMessage} />
+                <View style={{ flex: 1, flexDirection: 'row', alignItems: 'flex-end', alignSelf: 'stretch', backgroundColor: '#ff0000' }}>
+                    <TextInput style={{ alignSelf: 'stretch' }} value={this.state.newMessage} onChangeText={(text) => this.setState({ newMessage: text })} />
 
-                    <TouchableOpacity onPress={this.sendMessage}>
-                        <Text>Send</Text>
+                    <TouchableOpacity style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn} onPress={this.sendMessage}>
+                        <Text style={styles.submitBtnText}>Send</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -95,4 +100,50 @@ class Chat extends React.Component {
     }
 }
 
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: white
+    },
+    row: {
+      flexDirection: 'row',
+      flex: 1,
+      alignItems: 'center',
+    },
+    iosSubmitBtn: {
+      backgroundColor: purple,
+      padding: 10,
+      borderRadius: 7,
+      height: 45,
+      marginLeft: 40,
+      marginRight: 40,
+    },
+    AndroidSubmitBtn: {
+      backgroundColor: purple,
+      padding: 10,
+      paddingLeft: 30,
+      paddingRight: 30,
+      marginRight: 10,
+      height: 35,
+      borderRadius: 2,
+      alignSelf: 'flex-end',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    submitBtnText: {
+      color: white,
+      fontSize: 16,
+      textAlign: 'center',
+    },
+    center: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 30,
+      marginRight: 30,
+    },
+  })
+  
 export default Chat;
